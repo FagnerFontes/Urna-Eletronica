f762a482ba2325496090512e3cddb1030b19670c
Fagner Fontes Batista <fagnerfontesbatista@hotmail.com>
2023-12-18 11:31:50 -0300

    Projeto URNA

---
+++ etapa.js
@@ -0,0 +1,60 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: "38111",
+                name: "Fulano de Tal",
+                partido: "ABC",
+                fotos: [
+                    { url: "38111", legenda: "Vereador"}
+                ]
+            },
+            {
+                numero: "77222",
+                name: "Beltrano da Silva",
+                partido: "DEFG",
+                fotos: [
+                    { url: "77222", legenda: "Vereador"}
+                ]
+            },
+            {
+                 numero: "50005",
+                 name: "Fagner Fontes Batista", 
+                 partido: "NDSC",
+                 fotos:[
+                    
+                        { url: "50005", legenda: "Vereador"}
+                    
+                 ]
+            },    
+        ]
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: "99",
+                name: "Cicrano Souza",
+                partido: "ABC",
+                Vice: "CiC",
+                Fotos: [
+                    { url: "99.jpg", legenda: "Prefeito" },
+                    { url: "99_2.jpg", legenda: "Vice-Prefeito" }
+                ]
+            },
+            {
+                numero: "84",
+                name: "Ediel",
+                partido: "BNR",
+                Vice: "Edielson",
+                fotos: [
+                    { url: '84.jpg', legenda: 'Prefeito' },
+                    { url: '84_2.jpg', legenda: 'Vice-Prefeito' }
+                ]
+            }
+        ]
+    }
+]
